96fc0205f896c189fb5d44502443b19434d9b723
John Lima <ti.junior@gmail.com>
2021-08-14 16:39:00 -0300

    commit update

---
+++ app/src/main/java/br/com/multsoftware/v1/appbookny/data/model/Book.kt
@@ -0,0 +1,10 @@
+package br.com.multsoftware.v1.appbookny.data.model
+
+/**
+ * Created by João Bosco on 14/08/2021.
+ * e-mail - Support: ti.junior@gmail.com
+ */
+data class Book(
+    val title: String,
+    val author: String
+)
---
+++ app/src/main/java/br/com/multsoftware/v1/appbookny/presentation/books/BooksViewModel.kt
@@ -0,0 +1,10 @@
+package br.com.multsoftware.v1.appbookny.presentation.books
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by João Bosco on 14/08/2021.
+ * e-mail - Support: ti.junior@gmail.com
+ */
+class BooksViewModel : ViewModel() {
+}
