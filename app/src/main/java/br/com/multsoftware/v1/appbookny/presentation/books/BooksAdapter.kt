6b72d4a268d749c2627f6c8fb70af0e4f7f92d7b
John Lima <ti.junior@gmail.com>
2021-08-14 13:39:09 -0300

    commit update

---
+++ app/src/main/java/br/com/multsoftware/v1/appbookny/presentation/books/BooksAdapter.kt
@@ -0,0 +1,28 @@
+package br.com.multsoftware.v1.appbookny.presentation.books
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Created by João Bosco on 14/08/2021.
+ * e-mail - Support: ti.junior@gmail.com
+ */
+
+class BooksAdapter: RecyclerView.Adapter<BooksAdapter.BooksViewHolder>() {
+
+    class BooksViewHolder: RecyclerView.ViewHolder() {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BooksViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: BooksViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+}
