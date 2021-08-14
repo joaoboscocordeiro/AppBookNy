9342eb54b5b59f031423b71000149d1f60bd0979
John Lima <ti.junior@gmail.com>
2021-08-14 14:46:51 -0300

    commit update

--- app/src/main/java/br/com/multsoftware/v1/appbookny/presentation/books/BooksActivity.kt
+++ app/src/main/java/br/com/multsoftware/v1/appbookny/presentation/books/BooksActivity.kt
@@ -2,7 +2,10 @@ package br.com.multsoftware.v1.appbookny.presentation.books
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import br.com.multsoftware.v1.appbookny.R
+import br.com.multsoftware.v1.appbookny.data.model.Book
 import kotlinx.android.synthetic.main.activity_books.*
 
 class BooksActivity : AppCompatActivity() {
@@ -13,5 +16,21 @@ class BooksActivity : AppCompatActivity() {
 
         toolbar.title = getString(R.string.books_title)
         setSupportActionBar(toolbar)
+
+        with(recycler_books) {
+            layoutManager = LinearLayoutManager(this@BooksActivity, RecyclerView.VERTICAL, false)
+            setHasFixedSize(true)
+            adapter = BooksAdapter(getBooks())
+        }
+    }
+
+    // Moke
+    fun getBooks(): List<Book> {
+        return listOf(
+            Book("Title 01", "Author 01"),
+            Book("Title 02", "Author 02"),
+            Book("Title 03", "Author 03"),
+            Book("Title 04", "Author 04")
+        )
     }
 }
--- app/src/main/java/br/com/multsoftware/v1/appbookny/presentation/books/BooksAdapter.kt
+++ app/src/main/java/br/com/multsoftware/v1/appbookny/presentation/books/BooksAdapter.kt
@@ -1,28 +1,36 @@
 package br.com.multsoftware.v1.appbookny.presentation.books
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import br.com.multsoftware.v1.appbookny.R
+import br.com.multsoftware.v1.appbookny.data.model.Book
+import kotlinx.android.synthetic.main.item_book.view.*
 
 /**
  * Created by João Bosco on 14/08/2021.
  * e-mail - Support: ti.junior@gmail.com
  */
-
-class BooksAdapter: RecyclerView.Adapter<BooksAdapter.BooksViewHolder>() {
-
-    class BooksViewHolder: RecyclerView.ViewHolder() {
-
-    }
+class BooksAdapter(
+    private val books: List<Book>
+): RecyclerView.Adapter<BooksAdapter.BooksViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BooksViewHolder {
-
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_book, parent, false)
+        return BooksViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: BooksViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: BooksViewHolder, position: Int) =
+        holder.bindView(books[position])
 
-    }
+    override fun getItemCount(): Int = books.count()
 
-    override fun getItemCount(): Int {
+    class BooksViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
+        fun bindView(book: Book) {
+            itemView.text_title.text = book.title
+            itemView.text_author.text = book.author
+        }
     }
 }
